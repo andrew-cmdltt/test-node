69cf5f311d6714ef19c4f2a78585bb60383f3384
cmdltt@mail.ru <cmdltt@mail.ru>
2023-06-05 23:30:45 +0300

    work with geoserver

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express')
+import path from "path";
+import express from "express";
+
 const app = express()
+
 const port = 3000
 
-app.get('/', (req, res) => {
+function staticConnection(req, res) {
+    res.sendFile(path.resolve('./', 'dist', 'index.html'))
+}
+
+
+app.use(express.static('./dist'));
+
+app.get('/', staticConnection);
+
+
+
+app.get('/api', (req, res) => {
   console.log(req.get("host"));
   res.send('Hello World!')
 })
